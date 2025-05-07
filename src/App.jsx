8ab390aee956b28bfb31bb6186f391740fd32a8f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-07 22:23:50 +0530

    required code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+ const [firstName, setFirstName] = useState('')
+ const [lastName, setLastName] = useState('')
+ const [submitted, setSubmitted] = useState(false)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='form'>
+      <h1>Full Name Display</h1>
+      <form  onSubmit={(e) => e.preventDefault()}>
+        <label  htmlFor="firstName">
+          First Name:
+          <input
+          id='firstName'
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <label  htmlFor="lastName">
+          Last Name:
+          <input
+          id='lastName'
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <button
+          type="button"
+          onClick={() => {
+            if(firstName && lastName){setSubmitted(true)}
+            console.log(`First Name: ${firstName}, Last Name: ${lastName}`)
+          }}> Submit </button>
+      </form>
+      {submitted && (
+        <h5>Full Name: {firstName} {lastName}</h5>
+      )}
+    </div>
+  );
 }
 
 export default App
